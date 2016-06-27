b79a4b0e6e08faac473f275dbf8ca60f0c458063
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2016-06-27 17:09:38 -0500

    Update extmath.h

--- frmt/extmath.h
+++ frmt/extmath.h
@@ -20,7 +20,9 @@ typedef struct
 int toInt(float f);
 int cnvAngle(float theta);
 float toDegrees(float theta);
+vec3 dotm(vec3 a,vec3 r0,vec3 r1,vec3 r2);
 float dot(vec3 a,vec3 b);
+vec3 cross(vec3 a,vec3 b);
 vec3 normalize(vec3 v);
 vec3 hat(vec3 v);
 float reverse_angle(float c,float s);
