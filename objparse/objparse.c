5ed7793186d080fa6804973ccf903c4e16253cbe
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2016-07-10 15:17:53 -0500

    Fixed multitexture bug.

--- objparse/objparse.c
+++ objparse/objparse.c
@@ -3,7 +3,7 @@
 int tallyVertices = 0;
 int tallyTexCoords = 0;
 int tallyNormals = 0;
-int tallyObjs = 0;
+int tallyObjs = 1;
 int tallyObjNames = 0;
 int tallyMtls = 0;
 int errCount = 0;
@@ -49,42 +49,6 @@ void parseObj(char * objpath)
 			sscanf(param2,"%i/%i/%i",&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vb,&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vtb,&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vnb);
 			sscanf(param3,"%i/%i/%i",&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vc,&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vtc,&cmnObjs[tallyObjs-1].tris[tallyTris[tallyObjs-1]].vnc);
 			tallyTris[tallyObjs-1]++;
-		}
-		else if(strcmp(ident,"mtllib")==0) // MTL include identifier
-		{
-			sscanf(line," %s %s ",ident,param1);
-			FILE * mtl = fopen(param1,"r");
-			while(feof(mtl)==0)
-			{
-				char line2[80];
-				char ident2[80];
-				char params2[80];
-				fgets(line2,80,mtl);
-				sscanf(line2," %s %s ",ident2,params2);
-				if(strcmp(ident2,"newmtl")==0) // Material definition identifier
-				{
-					sscanf(params2," %s ",cmnMtls[tallyMtls].mtlname);
-				}
-				else if(strcmp(ident2,"map_Kd")==0) // Include image texture identifier
-				{
-					sscanf(params2," %s ",cmnMtls[tallyMtls].texname);
-					tallyMtls++;
-				}
-				else if(strcmp(ident2,"#")==0); // Comment identifier
-				else {printf("Syntax error in %s:\n %s %s\n",param1,ident2,params2); errCount++;}
-				for(int i=0; i<80; i++)
-				{
-					line2[i] = '\0';
-					ident2[i] = '\0';
-					params2[i] = '\0';
-				}
-			}
-		}
-		else if(strcmp(ident,"usemtl")==0) // MTL usage identifier
-		{
-			sscanf(line," %s %s ",ident,param1);
-			sscanf(param1," %s ",cmnObjs[tallyObjs].mat);
-			tallyObjs++;
 		}
 		else if(strcmp(ident,"o")==0) // Object name identifier
 		{
