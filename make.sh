419ad1c091ad00f8db55664a8eda4ac34189ddf4
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2016-10-09 15:20:02 -0500

    Animation support (complete)

---
+++ make.sh
@@ -0,0 +1,4 @@
+/home/jacob2/Desktop/games/smb/smbcnv_git_copy/main pinwheel.obj config.txt
+cd temp
+/home/jacob2/Desktop/games/smb/smbcnv_git_copy/lzdec/lzs output.lz.raw
+/home/jacob2/Desktop/games/smb/smbcnv_git_copy/lzdec/lzfix
